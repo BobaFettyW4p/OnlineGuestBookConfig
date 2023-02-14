cf62d677fe5de33d73eb0b285e4897c656b3fe95
Matthew Ivancic <111187499+BobaFettyW4p@users.noreply.github.com>
2023-02-14 07:30:23 -0600

    Update lamp.sh

--- lamp.sh
+++ lamp.sh
@@ -15,7 +15,7 @@ sudo chmod 2775 /var/www && find /var/www -type d -exec sudo chmod 2775 {} \;
 find /var/www -type f -exec sudo chmod 0664 {} \;
 
 #clone down config files
-git clone https://github.com/BobaFettyW4p/OnlineGuestBook.git
+git clone https://github.com/BobaFettyW4p/OnlineGuestBookConfig.git
 cd OnlineGuestBook/
 
 #make wsgi file executable
